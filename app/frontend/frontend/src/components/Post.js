76bc47be3c763c5a9fafde3bee9633fe3c78b4a5
Natalie Zharova <zharntl@yandex.ru>
2020-09-06 12:38:06 +0300

    update: front: задел под user

--- app/frontend/frontend/src/components/Post.js
+++ app/frontend/frontend/src/components/Post.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import User from './User';
 export default class Post extends Component {
     render() {
         return (
             <div className="post">
-
+                <User
+                    src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                    alt="man"
+                    name="Scott"
+                    min/>
                 <img src={this.props.src} alt={this.props.alt}></img>
                 <div className="post__name">
                     some account
