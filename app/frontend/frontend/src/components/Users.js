742458b402f550be272984e2a4b1e4643dd27598
Natalie Zharova <zharntl@yandex.ru>
2020-09-06 12:45:47 +0300

    update: front: задел под users

--- app/frontend/frontend/src/components/Users.js
+++ app/frontend/frontend/src/components/Users.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import User from './User';
 
 export default class Users extends Component {
     render() {
         return (
-            <div></div>
+            <div className="right">
+                <User
+                    src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                    alt="man"
+                    name="Scott" />
+                
+                <div className="users__block">
+                    <User
+                        src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                        alt="man"
+                        name="Scott"
+                        min />
+                    <User
+                        src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                        alt="man"
+                        name="Scott"
+                        min />
+                    <User
+                        src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                        alt="man"
+                        name="Scott"
+                        min />
+                    <User
+                        src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+                        alt="man"
+                        name="Scott"
+                        min />
+                </div>
+            </div>
         );
     }
 }
