f442b9b1cfe9be61cbccd793b750aea2304162a5
Natalie Zharova <zharntl@yandex.ru>
2020-09-06 16:17:17 +0300

    update: front: добавлена маршрутизация

--- app/frontend/frontend/src/components/User.js
+++ app/frontend/frontend/src/components/User.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const User = (props) => {
     const { min, src, alt, name } = props;
-    
+
     return (
         <a href="#" className={min ? "user min" : "user"}>
             <img src={src} alt={alt}></img>
